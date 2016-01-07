d3b7c4155a7b83983f57bbca6d4087162759bdc2
Li Li <litopia.li@gmail.com>
2016-01-07 13:37:05 -0800

    added validations for phone and email input types

--- src/scripts/validateForm.js
+++ src/scripts/validateForm.js
@@ -1,7 +1,57 @@
 module.exports = {
+    validatePhoneNumber: function(elem){
+        var errorMsg = this.fieldErrorMsg('Please enter a valid phone number. It should contains 10 digits and starts with your area code only.');
 
+        elem.addEventListener('blur', function(){
+            var phoneRegex = new RegExp('(\\d{3})(\\d{3})(\\d{4})');
+            var number = this.value;
+            number = number.replace(/[_\W]+/g, '');
+            var length = number.length;
+            
+            if(phoneRegex.test(number)){
+                this.value = number.replace(phoneRegex, "$1-$2-$3");
+                if(elem.classList.contains('invalid')){
+                    elem.classList.remove('invalid');
+                    elem.parentNode.removeChild(errorMsg);
+                    if(document.getElementsByClassName('invalid').length < 1){
+                        document.getElementById('submit').setAttribute('disabled', false);
+                    }
+                }
+            }else{
+                if (!elem.classList.contains('invalid')) {
+                    elem.classList.add('invalid');
+                    elem.parentNode.appendChild(errorMsg);
+                    document.getElementById('submit').setAttribute('disabled', true);
+                };
+            }
+            
+        })
+    },
     validateEmail: function (elem) {
         // Check email input against email format
+        var emailRegex = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$");
+        var that = this;
+        var errorMsg = that.fieldErrorMsg('This is not a valid email address.');
+
+        elem.addEventListener('blur', function(e){
+            var parent = elem.parentNode;
+
+            if(!emailRegex.test(this.value)){
+                if (!this.classList.contains('invalid')){
+                    this.classList.add('invalid');
+                    parent.appendChild(errorMsg);
+                    document.getElementById('submit').setAttribute('disabled', true);
+                };
+            }else{
+                if (this.classList.contains('invalid')) {
+                    this.classList.remove('invalid');
+                    parent.removeChild(errorMsg);
+                    if(document.getElementsByClassName('invalid').length < 1){
+                        document.getElementById('submit').setAttribute('disabled', false);
+                    }
+                };
+            }
+        })
     },
     initUpdateRelation: function(elem, targetID){
         var that = this;
@@ -15,8 +65,27 @@ module.exports = {
             targetElem.name = this.value;
         })
     },
-    initInsertRelation: function (elem, targetID) {
-        // body...
+    initInsertRelation: function (elem, target) {
+        // Show/hide subfields according to select value
+        // This funciton assumes that it always starts at 0
+        var that = this;
+
+        elem.addEventListener('change', function (e) {
+            var targetElems = that.getTargets(target);
+            var value = this.value;
+
+            for (var i = 0; i < targetElems.length; i++) {
+                if (i < value) {
+                    if(targetElems[i].classList.contains('hide')){
+                        targetElems[i].classList.remove('hide');
+                    }
+                }else{
+                    if(!targetElems[i].classList.contains('hide')){
+                        targetElems[i].classList.add('hide');
+                    }
+                }
+            };
+        })   
     },
     initConfirm: function(elem, targetID){
         var that = this;
@@ -54,6 +123,18 @@ module.exports = {
         msg.className = 'bform-errorMsg';
 
         return msg;
+    },
+    getTargets: function (selector) {
+        // Get targets by given selector
+        var firstChar = selector.charAt(0);
+
+        if(firstChar === '.'){
+            return document.getElementsByClassName(selector.substr(1));
+        }else if(firstChar === '#'){
+            return document.getElementById(selector.substr(1));
+        }
+
+        return false;
     },
     getClosest: function (elem, selector) {
         var firstChar = selector.charAt(0);
